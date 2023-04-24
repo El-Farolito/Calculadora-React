064485fa18c63dc88dbe623d87340c22f6ef8926
Lautaro Pedroza <Lautimotoe99@gmail.com>
2023-04-24 11:17:46 -0300

    creando la calculadora

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import logo from './imagenes/logo_large.png'
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+      
+
+        <img src={logo} className='logo-lautaropedroza' />
+
+        <div className='contenedor-calculadora'>
+
+        </div>
+
+
+    
+    </div>
+
+  );
+}
+
+export default App;

67ad49e1c0a06e1a78f1ad9d170ed2d88494120e
Lautaro Pedroza <Lautimotoe99@gmail.com>
2023-04-24 12:59:59 -0300

    creando la calculadora

--- src/componentes/boton.jsx
+++ src/componentes/boton.jsx
@@ -4,7 +4,7 @@ import '../css/boton.css'
 export function Boton(props) {
 
     const esOperador = valor =>{
-        return isNaN(valor) && (valor != '.') && (valor != '=');
+        return isNaN(valor) && (valor !== '.') && (valor !== '=');
     };
     return(
         <div className={`boton-contenedor ${esOperador(props.children)? 'operador': null}`}>
